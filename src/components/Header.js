b324d5fee665d8bfb21705ac36095f1c5e0da0dc
swarina <swarinajaiswal@gmail.com>
2021-07-05 14:37:35 +0530

    Button Component Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button 
+                color='green' 
+                text='Add'
+            />
         </header>
     )
 }
